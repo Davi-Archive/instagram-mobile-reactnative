9f6a94784e791e448ed817309727c63a270e2cb9
d1av <davi4alves@gmail.com>
2022-11-10 15:32:43 -0300

    feat: add search functionality and api call
    Fully functional search done, and stylized

--- src/_components/Container/Header/Search/index.tsx
+++ src/_components/Container/Header/Search/index.tsx
@@ -1,24 +1,92 @@
-import React, { useState } from "react";
-import { FlatList, View } from "react-native";
+import { useCallback, useEffect, useState } from "react";
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import { IUserData } from "../../../../_services/UserService/types";
-import styles from "./styles";
 import * as UserService from "../../../../_services/UserService";
+import { colors } from "../../../../../app.json";
+import styles from "./styles";
+import { useNavigation } from "@react-navigation/native";
+import Loading from "../../Loading";
 
 const Search = (props: { filter: string }) => {
+  const navigation = useNavigation<any>();
+
   const [users, setUsers] = useState<IUserData[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  const findUsers = async () => {
+  useEffect(() => {
+    setUsers([]);
+    if (props.filter.length > 2) {
+      findUsers();
+    }
+  }, [props.filter]);
+
+  const findUsers = useCallback(async () => {
     try {
       setIsLoading(true);
-      setUsers([]);
       const { data } = await UserService.search(props.filter);
+      const usersFormated: IUserData[] = data?.map(
+        (user: any, index: number) => {
+          const userFormated: IUserData = {
+            index: index,
+            id: user._id,
+            avatar: user.avatar,
+            email: user.email,
+            followers: user.seguidores,
+            following: user.seguindo,
+            posts: user.publicacoes,
+            name: user.nome
+          };
+          return userFormated;
+        }
+      );
 
-      const userForma
-    } catch (error) {}
-  };
+      setUsers(usersFormated);
+      setIsLoading(false);
+    } catch (err: any) {
+      setIsLoading(false);
+    }
+  }, [users]);
 
-  return <View style={styles.container}></View>;
+  const renderItem = (user: IUserData) => (
+    <TouchableOpacity
+      onPress={() => navigation.navigate("Profile", user)}
+      style={
+        user.index && user.index % 2 != 0
+          ? styles.backgroundOdd
+          : styles.backgroundPair
+      }>
+      <View style={styles.row}>
+        <View>
+          <Image
+            style={styles.imageUser}
+            source={
+              user.avatar
+                ? { uri: user.avatar }
+                : require("../../../../_assets/images/user.png")
+            }
+          />
+        </View>
+        <View>
+          <Text style={styles.name}>{user.name}</Text>
+          <Text style={styles.email}>{user.email}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+
+  return (
+    <View style={styles.container}>
+      {users.length > 0 && (
+        <FlatList
+          data={users}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => renderItem(item)}
+          onEndReachedThreshold={0.1}
+          ListFooterComponent={() => <Loading isLoading={isLoading} />}
+        />
+      )}
+    </View>
+  );
 };
 
 export default Search;
--- src/_components/Container/Header/Search/styles.ts
+++ src/_components/Container/Header/Search/styles.ts
@@ -1,12 +1,13 @@
 import { Dimensions, StyleSheet } from "react-native";
 import { colors,fonts } from '../../../../../app.json'
+
 const { height, width } = Dimensions.get("screen")
 
 const styles = StyleSheet.create({
     container: {
         zIndex: 9,
         position: 'absolute',
-        top: 90,
+        top: 62,
         backgroundColor: colors.whiteColor,
         alignItems: "center",
         justifyContent: "center"
